9b3345617302c8628cba689a99250b5e910110d8
Nikita-Solokha <147943333+Nikita-Solokha@users.noreply.github.com>
2026-04-28 23:02:13 +0300

    add ride-sharing project

---
+++ ride-sharing/services/payment-service/cmd/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"ride-sharing/services/payment-service/internal/events"
+	"ride-sharing/services/payment-service/internal/infrastructure/stripe"
+	"ride-sharing/services/payment-service/internal/service"
+	"ride-sharing/services/payment-service/pkg/types"
+	"ride-sharing/shared/env"
+	"ride-sharing/shared/messaging"
+	"ride-sharing/shared/tracing"
+)
+
+var GrpcAddr = env.GetString("GRPC_ADDR", ":9004")
+
+func main() {
+	tracerCfg := tracing.Config{
+		ServiceName:    "payment-service",
+		Environment:    env.GetString("ENVIRONMENT", "development"),
+		JaegerEndpoint: env.GetString("JAEGER_ENDPOINT", "http://jaeger:14268/api/traces"),
+	}
+
+	sh, err := tracing.InitTracer(tracerCfg)
+	if err != nil {
+		log.Fatalf("Failed to initialize the tracer: %v", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	defer sh(ctx)
+
+	rabbitMqURI := env.GetString("RABBITMQ_URI", "amqp://guest:guest@rabbitmq:5672/")
+
+	go func() {
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+		<-sigCh
+		cancel()
+	}()
+
+	appURL := env.GetString("APP_URL", "http://localhost:3000")
+
+	stripeCfg := &types.PaymentConfig{
+		StripeSecretKey: env.GetString("STRIPE_SECRET_KEY", ""),
+		SuccessURL:      env.GetString("STRIPE_SUCCESS_URL", appURL+"?payment=success"),
+		CancelURL:       env.GetString("STRIPE_CANCEL_URL", appURL+"?payment=cancel"),
+	}
+
+	if stripeCfg.StripeSecretKey == "" {
+		log.Fatalf("STRIPE_SECRET_KEY is not set")
+		return
+	}
+
+	paymentProcessor := stripe.NewStripeClient(stripeCfg)
+
+	svc := service.NewPaymentService(paymentProcessor)
+
+	rabbitmq, err := messaging.NewRabbitMQ(rabbitMqURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rabbitmq.Close()
+
+	log.Println("Starting RabbitMQ connection")
+
+	tripConsumer := events.NewTripConsumer(rabbitmq, svc)
+	go tripConsumer.Listen()
+
+	<-ctx.Done()
+	log.Println("Shutting down payment service...")
+}
